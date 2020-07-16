46bb1101e8b8f146116f5d4c1e54f82abc093226
Amir Razmjou <arazmj>
2020-07-16 16:05:41 -0400

    Updated flag descriptions

--- README.md
+++ README.md
@@ -3,11 +3,10 @@
 GoCache is a key value in-memory database server written in GoLang 
 
 ```
-Usage of ./GoCache:
-  -capacity int
-        how big the cache will be, the old values will be evicted, default is 100 (default 100)
+ -capacity int
+        how big the cache will be, the old values will be evicted (default 100)
   -port int
-        the server port number, default is 8080 (default 8080)
+        the server port number. (default 8080)
 ```
 
 Example of usage:
--- gocache.go
+++ gocache.go
@@ -12,22 +12,22 @@ import (
 var cache LRUCache.LRUCache
 
 func main() {
-	capacity := flag.Int("capacity", 100, "how big the cache will be, the old values will be evicted, default is 100")
-	port := flag.Int("port", 8080, "the server port number, default is 8080")
+	capacity := flag.Int("capacity", 100, "how big the cache will be, the old values will be evicted")
+	port := flag.Int("port", 8080, "the server port number.")
 	flag.Parse()
 	cache = LRUCache.NewCache(*capacity)
 	router := mux.NewRouter()
 	router.HandleFunc("/cache/{key}", GetHandler).Methods(http.MethodGet)
 	router.HandleFunc("/cache/{key}/{value}", PutHandler).Methods(http.MethodPost)
 	log.Printf("GoCache started listening on %d port\n", *port)
-	log.Fatal(http.ListenAndServe(":" + strconv.Itoa(*port), router))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), router))
 }
 
 func PutHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["key"]
 	value := vars["value"]
-	cache.Put(key,value)
+	cache.Put(key, value)
 	w.WriteHeader(http.StatusOK)
 }
 
@@ -36,7 +36,7 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 	key := vars["key"]
 	if value, ok := cache.Get(key); ok {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(value))
+		_, _ = w.Write([]byte(value))
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 	}
