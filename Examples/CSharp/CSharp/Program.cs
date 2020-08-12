a1b3de7a6a098aaa43664eee5aa307eb8c2830fa
Amir Razmjou <arazmj>
2020-08-12 18:52:38 -0400

    Added an example for C#

---
+++ Examples/CSharp/CSharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods.Http;
+
+namespace CSharp
+{
+    class Program
+    {
+        private static string _hostname = "http://localhost";
+        private static string _port = "8080";
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            client.PostAsync($"{_hostname}:{_port}/cache/Hello/World", 
+                new StringContent("")).Wait();
+
+            var response = client.GetAsync($"{_hostname}:{_port}/cache/Hello")
+                .Result;
+            
+            if (response != null)
+            {
+                var value = string.Empty;
+
+                Task task = response.Content.ReadAsStreamAsync().ContinueWith(t =>
+                {
+                    var stream = t.Result;
+                    using (var reader = new StreamReader(stream))
+                    {
+                        value = reader.ReadToEnd();
+                    }
+                });
+
+                task.Wait();
+                Console.WriteLine($"Hello = {value}");
+            }
+
+        }
+    }
+}
