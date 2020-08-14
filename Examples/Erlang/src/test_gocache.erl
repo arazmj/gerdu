4ab519ee6bcd668a90c61b4fc5b74e769c3e3ee6
Amir Razmjou <arazmj>
2020-08-14 03:30:56 -0400

    Added Erlang example

---
+++ Examples/Erlang/src/test_gocache.erl
@@ -0,0 +1,19 @@
+%%%-------------------------------------------------------------------
+%%% @author Amir Razmjou
+%%% @copyright (C) 2020
+%%% Created : 14. Aug 2020 2:52 AM
+%%%-------------------------------------------------------------------
+-module(test_gocache).
+-author("arazmj@gmail.com").
+
+%% API
+-export([test/0]).
+
+test() ->
+  inets:start(),
+  Url = "http://localhost:8080/cache/Hello",
+  httpc:request(put, {Url, [],[], "World"}, [], []),
+  {ok, {{_, _, _}, _, Body}} = httpc:request(get, {Url, []}, [], []),
+  erlang. "Hello = " ++ Body.
+
+
