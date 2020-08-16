9537b424d0f7b13360780632fed5a86f4f1ad8ba
Amir Razmjou <arazmj>
2020-08-15 23:26:41 -0400

    Added example gRPC Python

---
+++ examples/gRPC/Python/main.py
@@ -0,0 +1,14 @@
+# Gerdu gRPC Python Example
+# Amir Razmjou 2020
+
+from proto.gerdu_pb2 import *
+from proto.gerdu_pb2_grpc import *
+
+if __name__ == '__main__':
+    channel = grpc.insecure_channel('localhost:8081')
+    stub = GerduStub(channel)
+    stub.Put(PutRequest(key="Hello", value=b"World"))
+    response = stub.Get(GetRequest(key="Hello"))
+    values = response.value.decode("utf-8")
+    print("Hello =", values)
+
